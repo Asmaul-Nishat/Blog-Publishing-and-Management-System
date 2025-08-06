59823311fb2e3db15ad23106f82a8c7c60d09194
Asmaul Nishat <nishatsoyati@gmail.com>
2025-08-06 10:19:54 +0600

    updated db

--- sql/new-blog-database.sql
+++ sql/new-blog-database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 05, 2025 at 11:55 AM
+-- Generation Time: Aug 06, 2025 at 06:03 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -85,7 +85,32 @@ INSERT INTO `comments` (`id`, `user_id`, `username`, `post_id`, `parent_id`, `co
 (3, 7, 'Asmaul', 15, NULL, 'hello', 0, '2025-08-05 15:39:22'),
 (4, 7, 'Asmaul', 15, NULL, 'welcome', 0, '2025-08-05 15:39:31'),
 (5, 7, 'Asmaul', 15, NULL, 'view the post', 0, '2025-08-05 15:39:41'),
-(6, 7, 'Asmaul', 15, NULL, 'nooo', 0, '2025-08-05 15:39:55');
+(6, 7, 'Asmaul', 15, NULL, 'nooo', 0, '2025-08-05 15:39:55'),
+(7, 7, 'Asmaul', 15, NULL, 'noo', 0, '2025-08-05 16:44:43'),
+(8, 7, 'Asmaul', 15, NULL, 'hiii', 0, '2025-08-05 16:44:59'),
+(9, 7, 'Asmaul', 8, NULL, 'Read...', 0, '2025-08-06 08:33:22'),
+(10, 7, 'Asmaul', 12, NULL, 'heal!', 0, '2025-08-06 08:43:08'),
+(11, 7, 'Asmaul', 15, NULL, 'ho', 0, '2025-08-06 09:07:29'),
+(12, 7, 'Asmaul', 15, NULL, 'nothing', 0, '2025-08-06 09:07:45'),
+(13, 7, 'Asmaul', 15, NULL, 'ni', 0, '2025-08-06 09:07:53'),
+(14, 7, 'Asmaul', 8, NULL, 'noo', 0, '2025-08-06 09:17:54'),
+(15, 7, 'Asmaul', 11, NULL, 'no', 0, '2025-08-06 09:34:28'),
+(16, 7, 'Asmaul', 10, NULL, 'Read doneno', 0, '2025-08-06 09:36:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment_edits`
+--
+
+CREATE TABLE `comment_edits` (
+  `id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  `old_comment` text NOT NULL,
+  `new_comment` text NOT NULL,
+  `edited_by` int(11) NOT NULL,
+  `edited_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -137,7 +162,13 @@ CREATE TABLE `likes` (
 
 INSERT INTO `likes` (`id`, `user_id`, `username`, `post_id`, `created_at`) VALUES
 (18, 6, 'Soyati Nishat', 13, '2025-08-05 07:44:46'),
-(46, 7, 'Asmaul', 15, '2025-08-05 09:39:08');
+(47, 7, 'Asmaul', 9, '2025-08-05 10:19:45'),
+(50, 7, 'Asmaul', 14, '2025-08-06 02:40:05'),
+(51, 7, 'Asmaul', 12, '2025-08-06 02:41:06'),
+(52, 7, 'Asmaul', 15, '2025-08-06 03:07:17'),
+(53, 7, 'Asmaul', 8, '2025-08-06 03:17:42'),
+(54, 7, 'Asmaul', 11, '2025-08-06 03:34:25'),
+(55, 7, 'Asmaul', 10, '2025-08-06 03:36:54');
 
 -- --------------------------------------------------------
 
@@ -165,14 +196,14 @@ CREATE TABLE `posts` (
 
 INSERT INTO `posts` (`id`, `user_id`, `title`, `content`, `image`, `category_id`, `status`, `created_at`, `updated_at`, `views`, `author`) VALUES
 (1, NULL, 'World', 'world is...', '1753918802_alice-alinari-MS371wlcGPo-unsplash.jpg', NULL, 'published', '2025-07-30 23:40:02', '2025-08-05 09:30:27', 4, 'Admin'),
-(8, NULL, 'Looking into the heart', 'It starts with building a relationship when I need to describe myself to someone who can’t see me. Honestly, I’m not into describing myself to someone in just a few words, as my whole being', '1754010368_freestocks-OfaDD5o8hpk-unsplash.jpg', NULL, 'published', '2025-08-01 01:06:08', '2025-08-05 05:09:21', 3, 'Admin'),
+(8, NULL, 'Looking into the heart', 'It starts with building a relationship when I need to describe myself to someone who can’t see me. Honestly, I’m not into describing myself to someone in just a few words, as my whole being', '1754010368_freestocks-OfaDD5o8hpk-unsplash.jpg', NULL, 'published', '2025-08-01 01:06:08', '2025-08-06 03:31:34', 11, 'Admin'),
 (9, NULL, 'Hearth', 'The still wind serenading the hillssweet rain that drenches the window sillsan echo of joy whispering through the nighta soothing...', '1754010452_aaron-burden-xG8IQMqMITM-unsplash.jpg', NULL, 'published', '2025-08-01 01:07:32', '2025-08-02 00:00:59', 2, 'Admin'),
-(10, NULL, 'The World', 'Travel is the movement of people between different locations, either for leisure or other purposes. It can be done through various modes of transportation like walking, vehicles, or airplanes. The term can also refer to the act of going on a trip, often to a distant place.', '1754010968_karsten-winegeart-Quh5YoaAzfI-unsplash.jpg', 3, 'published', '2025-08-01 01:16:08', '2025-08-04 11:08:04', 14, 'Admin'),
-(11, NULL, 'Bucket List Travel: The Top 20 Places In The World!', 'If you’re like most people, the allure of new horizons and uncharted adventures keeps you constantly adding destinations to your travel bucket list. Kuoni—a Britain-based travel company— recently did a study to rank the world’s best bucket list destinations. Kuoni conducted the comprehensive analysis by scouring Google search data for 119 bucket list places to visit in 219 countries. The results paint a vivid picture of travel aspirations, spanning from the breathtaking shores of the Maldives to the thundering majesty of Niagara Falls, encompassing iconic landmarks and awe-inspiring natural wonders along the way.\r\n\r\nAccording to Sheena Paton, senior program manager, this is the first time Kuoni has assembled this travel bucket list . “We’ve previously created travel stories that have analyzed the demand for different locations or the best landmarks to visit, but this unique study looks at every country&#039;s most popular bucket list item,” Paton told me in an interview. “We were intrigued to see how this might vary from country to country and which experiences would come out on top, so we decided to do this study to reveal what travelers around the world want to experience.”\r\n\r\nmaldives top bucket list trip Coming in at the top of the world’s best bucket list destinations is the idyllic cluster of islands known as the Maldives—which is known for its stunning white sands, bright blue waters and incredible sunsets. It topped the travel bucket list for 121 countries. “It’s great to see that a trip to the Maldives is considered a bucket list item for so many people,” says Paton. “It’s a tropical paradise popular amongst honeymooners, families and retirees alike.” Coincidentally, in 2021 Kuoni looked at where the world wants to travel most. “The top destinations to travel to that year were the Maldives, Mexico and Bali, which is interesting because visiting the Maldives also came top as the most searched-for travel bucket list experience in this most recent report,” Paton explains.', '1754093541_960x0.jpg', 3, 'published', '2025-08-02 00:12:21', '2025-08-02 01:02:41', 7, 'Admin'),
-(12, NULL, 'Petra, Jordan', 'this city on the edge of the Wadi Araba was quite literally carved into the rose-colored sandstone of a deep canyon.\r\n\r\nCreated by the Nabateans, Petra (“stone” in Greek) controlled trade routes stretching from Africa to India and China. The Romans later conquered the area and it was eventually abandoned and forgotten until a Swiss explorer rediscovered it in 1812.\r\n\r\nFeatured in Indiana Jones and the Last Crusade, Petra’s famous Treasury was painstakingly chiseled from sand and stone. The hike to Petra’s magnificent Monastery is another must-do.', '1754096374_The-Monastery-Petra-Jordan.jpg', 3, 'published', '2025-08-02 00:59:34', '2025-08-05 04:55:03', 2, 'Admin'),
-(13, NULL, 'Victoria Falls, Zambia/Zimbabwe!!', 'The UNESCO World Heritage site of Victoria Falls is the largest curtain of falling water in the world. More than twice the height of Niagara Falls, it’s matched only by Iguazu Falls in South America.\r\nExplorer David Livingstone is believed to have been the first European to view Victoria Falls in 1855. Livingstone named his discovery after Queen Victoria but the indigenous name, is Mosi-oa-Tunya, literally, “the smoke that thunders.”\r\n\r\nDuring the high water season (February to May) more than 19 million cubic feet of water plummets over the edge and the falls are at their most dramatic. Bonus – it’s rainbow season! \r\n\r\nDuring the dry season from June to January, the falls often dwindle to a trickle. The shoulder season between high and low is an ideal time for viewing (I visited in February and it was fabulous).', '1754097338_pars-sahin-NMFulKCYrkY-unsplash.jpg', 3, 'published', '2025-08-02 01:15:38', '2025-08-05 07:44:54', 5, 'Admin'),
-(14, 6, 'Soyati', 'Nothing added', 'post_688d78c30d0d6.jpg', 1, 'published', '2025-08-02 02:32:35', '2025-08-05 08:03:44', 14, 'Admin'),
-(15, NULL, 'Wonder the world', 'Element represents a section of a page whose purpose is to provide navigation links, either within the current document or to other documents. Common examples of navigation sections are menus, tables of contents, and indexes.', '1754176956_alexandra-tran-YvIXIBW6bJk-unsplash.jpg', 3, 'published', '2025-08-02 23:22:36', '2025-08-05 09:53:03', 59, 'Admin');
+(10, NULL, 'The World', 'Travel is the movement of people between different locations, either for leisure or other purposes. It can be done through various modes of transportation like walking, vehicles, or airplanes. The term can also refer to the act of going on a trip, often to a distant place.', '1754010968_karsten-winegeart-Quh5YoaAzfI-unsplash.jpg', 3, 'published', '2025-08-01 01:16:08', '2025-08-06 03:36:59', 18, 'Admin'),
+(11, NULL, 'Bucket List Travel: The Top 20 Places In The World!', 'If you’re like most people, the allure of new horizons and uncharted adventures keeps you constantly adding destinations to your travel bucket list. Kuoni—a Britain-based travel company— recently did a study to rank the world’s best bucket list destinations. Kuoni conducted the comprehensive analysis by scouring Google search data for 119 bucket list places to visit in 219 countries. The results paint a vivid picture of travel aspirations, spanning from the breathtaking shores of the Maldives to the thundering majesty of Niagara Falls, encompassing iconic landmarks and awe-inspiring natural wonders along the way.\r\n\r\nAccording to Sheena Paton, senior program manager, this is the first time Kuoni has assembled this travel bucket list . “We’ve previously created travel stories that have analyzed the demand for different locations or the best landmarks to visit, but this unique study looks at every country&#039;s most popular bucket list item,” Paton told me in an interview. “We were intrigued to see how this might vary from country to country and which experiences would come out on top, so we decided to do this study to reveal what travelers around the world want to experience.”\r\n\r\nmaldives top bucket list trip Coming in at the top of the world’s best bucket list destinations is the idyllic cluster of islands known as the Maldives—which is known for its stunning white sands, bright blue waters and incredible sunsets. It topped the travel bucket list for 121 countries. “It’s great to see that a trip to the Maldives is considered a bucket list item for so many people,” says Paton. “It’s a tropical paradise popular amongst honeymooners, families and retirees alike.” Coincidentally, in 2021 Kuoni looked at where the world wants to travel most. “The top destinations to travel to that year were the Maldives, Mexico and Bali, which is interesting because visiting the Maldives also came top as the most searched-for travel bucket list experience in this most recent report,” Paton explains.', '1754093541_960x0.jpg', 3, 'published', '2025-08-02 00:12:21', '2025-08-06 03:41:17', 14, 'Admin'),
+(12, NULL, 'Petra, Jordan', 'this city on the edge of the Wadi Araba was quite literally carved into the rose-colored sandstone of a deep canyon.\r\n\r\nCreated by the Nabateans, Petra (“stone” in Greek) controlled trade routes stretching from Africa to India and China. The Romans later conquered the area and it was eventually abandoned and forgotten until a Swiss explorer rediscovered it in 1812.\r\n\r\nFeatured in Indiana Jones and the Last Crusade, Petra’s famous Treasury was painstakingly chiseled from sand and stone. The hike to Petra’s magnificent Monastery is another must-do.', '1754096374_The-Monastery-Petra-Jordan.jpg', 3, 'published', '2025-08-02 00:59:34', '2025-08-06 03:20:00', 5, 'Admin'),
+(13, NULL, 'Victoria Falls, Zambia/Zimbabwe!!', 'The UNESCO World Heritage site of Victoria Falls is the largest curtain of falling water in the world. More than twice the height of Niagara Falls, it’s matched only by Iguazu Falls in South America.\r\nExplorer David Livingstone is believed to have been the first European to view Victoria Falls in 1855. Livingstone named his discovery after Queen Victoria but the indigenous name, is Mosi-oa-Tunya, literally, “the smoke that thunders.”\r\n\r\nDuring the high water season (February to May) more than 19 million cubic feet of water plummets over the edge and the falls are at their most dramatic. Bonus – it’s rainbow season! \r\n\r\nDuring the dry season from June to January, the falls often dwindle to a trickle. The shoulder season between high and low is an ideal time for viewing (I visited in February and it was fabulous).', '1754097338_pars-sahin-NMFulKCYrkY-unsplash.jpg', 3, 'published', '2025-08-02 01:15:38', '2025-08-06 03:19:55', 10, 'Admin'),
+(14, 6, 'Soyati', 'Nothing added', 'post_688d78c30d0d6.jpg', 1, 'published', '2025-08-02 02:32:35', '2025-08-06 02:40:06', 16, 'Admin'),
+(15, NULL, 'Wonder the world', 'Element represents a section of a page whose purpose is to provide navigation links, either within the current document or to other documents. Common examples of navigation sections are menus, tables of contents, and indexes.', '1754176956_alexandra-tran-YvIXIBW6bJk-unsplash.jpg', 3, 'published', '2025-08-02 23:22:36', '2025-08-06 03:40:02', 123, 'Admin');
 
 -- --------------------------------------------------------
 
@@ -195,9 +226,10 @@ CREATE TABLE `ratings` (
 
 INSERT INTO `ratings` (`id`, `user_id`, `username`, `post_id`, `rating`, `created_at`) VALUES
 (1, 6, 'Soyati Nishat', 15, 5, '2025-08-05 07:33:07'),
-(4, 6, 'Soyati Nishat', 13, 5, '2025-08-05 07:49:30'),
 (10, 6, 'Soyati Nishat', 1, 3, '2025-08-05 09:30:30'),
-(13, 7, 'Asmaul', 15, 5, '2025-08-05 09:40:04');
+(13, 7, 'Asmaul', 15, 5, '2025-08-05 09:40:04'),
+(40, 7, 'Asmaul', 10, 4, '2025-08-06 03:36:32'),
+(46, 7, 'Asmaul', 11, 3, '2025-08-06 03:41:13');
 
 -- --------------------------------------------------------
 
@@ -282,6 +314,14 @@ ALTER TABLE `comments`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `comment_edits`
+--
+ALTER TABLE `comment_edits`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `comment_id` (`comment_id`),
+  ADD KEY `edited_by` (`edited_by`);
+
+--
 -- Indexes for table `contact_messages`
 --
 ALTER TABLE `contact_messages`
@@ -353,7 +393,13 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `comment_edits`
+--
+ALTER TABLE `comment_edits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `contact_messages`
@@ -365,7 +411,7 @@ ALTER TABLE `contact_messages`
 -- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT for table `posts`
@@ -377,7 +423,7 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `ratings`
 --
 ALTER TABLE `ratings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT for table `settings`
@@ -414,6 +460,13 @@ ALTER TABLE `analytics`
 ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `comment_edits`
+--
+ALTER TABLE `comment_edits`
+  ADD CONSTRAINT `comment_edits_ibfk_1` FOREIGN KEY (`comment_id`) REFERENCES `comments` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comment_edits_ibfk_2` FOREIGN KEY (`edited_by`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `contact_messages`
